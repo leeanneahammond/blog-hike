8ebd26a989e9f8c501f224de3b18862f2ecb1770
Leeanne Hammond <leeannehammond@yahoo.com>
2020-05-26 18:09:49 -0400

    login working

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const session = require('express-session');
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const _ = require("lodash");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 const homeStartingContent = "";
 const aboutContent = "I'm baby godard sustainable semiotics mustache artisan shabby chic fixie farm-to-table tacos. Fixie portland art party cardigan schlitz. Poutine chambray bitters, tilde enamel pin deep v locavore small batch lomo keffiyeh edison bulb migas blog semiotics craft beer. Roof party pitchfork kogi, before they sold out cornhole trust fund echo park. Glossier man braid tofu affogato activated charcoal keytar organic semiotics selfies ugh lumbersexual. Tofu sartorial banh mi yuccie retro asymmetrical ramps semiotics bicycle rights enamel pin cornhole artisan prism brooklyn ennui. Shabby chic craft beer lyft offal echo park, snackwave glossier banjo organic gluten-free disrupt brunch meggings leggings.";
 const contactContent = "";
@@ -12,10 +17,22 @@ const vaContent = "Click trails to view more information.";
 
 const app = express();
 
-app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(session({
+  secret: "Our little secret.",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 mongoose.connect("mongodb+srv://leeanneah:abcdef1@cluster0-ejfqj.mongodb.net/test?retryWrites=true&w=majority",
   {
@@ -23,6 +40,124 @@ mongoose.connect("mongodb+srv://leeanneah:abcdef1@cluster0-ejfqj.mongodb.net/tes
     useUnifiedTopology: true,
     useCreateIndex: true
   });
+mongoose.set("useCreateIndex", true);
+
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+  googleId: String,
+  secret: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+
+app.get("/", function (req, res) {
+  res.render("home");
+});
+
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+
+app.get("/register", function (req, res) {
+  res.render("register");
+});
+
+app.get("/secrets", function (req, res) {
+  User.find({ "secret": { $ne: null } }, function (err, foundUsers) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUsers) {
+        res.render("secrets", { usersWithSecrets: foundUsers });
+      }
+    }
+  });
+});
+
+app.get("/submit", function (req, res) {
+  if (req.isAuthenticated()) {
+    res.render("submit");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.post("/submit", function (req, res) {
+  const submittedSecret = req.body.secret;
+
+  //Once the user is authenticated and their session gets saved, their user details are saved to req.user.
+  // console.log(req.user.id);
+
+  User.findById(req.user.id, function (err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        foundUser.secret = submittedSecret;
+        foundUser.save(function () {
+          res.redirect("/secrets");
+        });
+      }
+    }
+  });
+});
+
+app.get("/logout", function (req, res) {
+  req.logout();
+  res.redirect("/");
+});
+
+app.post("/register", function (req, res) {
+
+  User.register({ username: req.body.username }, req.body.password, function (err, user) {
+    if (err) {
+      console.log(err);
+      res.redirect("/register");
+    } else {
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/secrets");
+      });
+    }
+  });
+
+});
+
+app.post("/login", function (req, res) {
+
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  req.login(user, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/secrets");
+      });
+    }
+  });
+
+});
 
 const postSchema = {
   title: String,
